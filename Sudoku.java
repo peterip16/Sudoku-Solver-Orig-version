336150fc3dd2604b3e01bf09a964caed6bdbcb98
peterip16 <peterip16@gmail.com>
2017-08-02 15:09:40 -0700

    Created the files.

---
+++ Sudoku.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Pak Ho
+ */
+
+public class Sudoku {
+    
+    int[][] sudoku;
+    
+    public Sudoku(){
+        sudoku = new int[10][10];
+    }
+    
+    public Sudoku(int[][] sudoku){
+        this.sudoku = sudoku;
+    }
+}
